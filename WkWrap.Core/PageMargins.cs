108796ce59b6670e9cfafbbdb97fd172562c9500
Roman Bukin <nomoreload@gmail.com>
2017-03-11 13:11:23 +0300

    Add solution.

---
+++ WkWrap.Core/PageMargins.cs
@@ -0,0 +1,56 @@
+﻿namespace WkWrap.Core
+{
+    /// <summary>
+    /// Struct that represents PDF page margins.
+    /// </summary>
+    public struct PageMargins
+    {
+        /// <summary>
+        /// Returns new instance of <see cref="PageMargins"/> with default margins.
+        /// </summary>
+        /// <returns></returns>
+        public static PageMargins Default()
+        {
+            return new PageMargins();
+        }
+
+        /// <summary>
+        /// Constructs new instance of <see cref="PageMargins"/>.
+        /// </summary>
+        /// <param name="left">PDF page left margin (in mm).</param>
+        /// <param name="top">PDF page top margin (in mm).</param>
+        /// <param name="right">PDF page right margin (in mm).</param>
+        /// <param name="bottom">PDF page bottom margin (in mm).</param>
+        public PageMargins(
+            double? left = null,
+            double? top = null,
+            double? right = null,
+            double? bottom = null)
+        {
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+
+        /// <summary>
+        /// Returns PDF page left margin (in mm).
+        /// </summary>
+        public double? Left { get; }
+
+        /// <summary>
+        /// Returns PDF page top margin (in mm).
+        /// </summary>
+        public double? Top { get; }
+
+        /// <summary>
+        /// Returns PDF page right margin (in mm).
+        /// </summary>
+        public double? Right { get; }
+
+        /// <summary>
+        /// Returns PDF page bottom margin (in mm).
+        /// </summary>
+        public double? Bottom { get; }
+    }
+}
---
+++ WkWrap.Core/WkWrapException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WkWrap.Core
+{
+    /// <summary>
+    /// The exception that thrown when WkHtmlToPdf process retruns non-zero exit code.
+    /// </summary>
+    public class WkWrapException : Exception
+    {
+        /// <summary>
+        /// Gets WkHtmlToPdf process error code.
+        /// </summary>
+        public int ErrorCode { get; }
+
+        /// <summary>
+        /// Returns new instance of <see cref="WkWrapException"/>.
+        /// </summary>
+        /// <param name="errorCode">WkHtmlToPdf process error code.</param>
+        /// <param name="message">WkHtmlToPdf error text.</param>
+        public WkWrapException(int errorCode, string message) : base($"{message} ({errorCode:D})")
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
